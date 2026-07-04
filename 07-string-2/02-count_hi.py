309078124c27292bb473547014aa64d07d6de620
sejjy <sejjymvm@gmail.com>
2026-07-04 12:13:18 +0800

    add string-2 solutions

---
+++ 07-string-2/02-count_hi.py
@@ -0,0 +1,26 @@
+# Return the number of times that the string "hi" appears anywhere in the given
+# string.
+#
+# count_hi('abc hi ho') → 1
+# count_hi('ABChi hi') → 2
+# count_hi('hihi') → 2
+
+
+def count_hi(str):
+  count = 0
+  for i in range(len(str) - 1):
+    if (str[i] + str[i + 1]) == 'hi':
+      count += 1
+  return count
+
+
+# Solution:
+
+# def count_hi(str):
+#   sum = 0
+#   ## Loop to length-1 and access index i and i+1
+#   ## in the loop.
+#   for i in range(len(str)-1):
+#     if str[i:i+2] == 'hi':
+#       sum = sum + 1
+#   return sum
---
+++ 07-string-2/03-cat_dog.py
@@ -0,0 +1,17 @@
+# Return True if the string "cat" and "dog" appear the same number of times in
+# the given string.
+#
+# cat_dog('catdog') → True
+# cat_dog('catcat') → False
+# cat_dog('1cat1cadodog') → True
+
+
+def cat_dog(str):
+  count_cat = 0
+  count_dog = 0
+  for i in range(len(str) - 2):
+    if str[i : i + 3] == 'cat':
+      count_cat += 1
+    if str[i : i + 3] == 'dog':
+      count_dog += 1
+  return count_cat == count_dog
